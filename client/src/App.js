6094e7c241358f896a3fab141acd16c14c5fdc02
sarath <sarathsarath93366@gmail.com>
2024-02-18 11:44:05 +0530

    router probelem solved

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,32 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home/Home';
+import { HashRouter, Routes, Route } from "react-router-dom"
 
 function App() {
- 
- 
- 
+
+
+
   return (
     <div>
 
-    <Home />
-      
+
+     
+        <Routes >
+
+          <Route element={<Home />} path='/' />
+
+
+        </Routes>
+
+     
+
+
+
+
+
+
+
     </div>
   )
 }
